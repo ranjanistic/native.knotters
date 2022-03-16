44bfe99190f80778aef738d224c3fc685a304ed2
ranjanistic <priyanshuranjan88@gmail.com>
2022-03-17 02:11:23 +0530

    create env

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { WebView } from 'react-native-webview';
 
+console.log(process.env.URL)
+
 export default function App() {
   return (
     <WebView
-      source={{ uri: "https://knotters.org" }}
+      source={{ uri: process.env.URL }}
       style={{ marginTop: 40 }}
+      allowFileAccess={false}
+      allowsBackForwardNavigationGestures={true}
     />
   );
 }
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+# Knative
+
+Knotters native application using react-native webview.
+
+## Setup project
+
+Create a .env file in project's root using reference from the existing [.env.example](.env.example) file.
+
+## Install dependencies
+
 ```bash
 yarn
 ```
+
+## Run application
 
 ```bash
 yarn start
