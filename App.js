4bdae7d6cf446cbb1b1023ae87d372de1f6a9b90
ranjanistic <priyanshuranjan88@gmail.com>
2022-03-18 04:00:55 +0530

    update package json

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {SafeAreaView, StatusBar} from 'react-native';
+
 import {WebView} from 'react-native-webview';
 
-export default App = () => (
-  <WebView
-    source={{uri: process.env.URL}}
-    style={{marginTop: 40}}
-    allowFileAccess={false}
-    allowsBackForwardNavigationGestures={true}
-  />
-);
+const App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={'light-content'} />
+      <WebView
+        source={{uri: process.env.URL}}
+        style={{marginTop: 40}}
+        allowFileAccess={false}
+        allowsBackForwardNavigationGestures={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
